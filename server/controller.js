e2bebb48ff0a9757aea1d174e1a2431fbac9613b
Collin Rentz <rentzcollin@gmail.com>
2022-10-07 08:12:30 -0600

    start

---
+++ server/controller.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["You will soon contract an unpleasant disease.", "Your fly in down.", "You will find a surprise on your doorstep when you return home.", "Watch a movie with a significant other or loved one, you will be rewarded with a holy kiss.", "Check your back pocket, if there is nothing there then you gave yourself a nice pat on the bum, congrats."];
+      
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomFortune = compliments[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
